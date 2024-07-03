1395071d52534afd563b5418a13f7fbd168523ca
Lisa <L.carpenter@outlook.com>
2024-07-03 14:17:33 +0100

    Create obtaindata.py

---
+++ obtaindata.py
@@ -0,0 +1,33 @@
+import requests 
+from datetime import datetime, timedelta 
+import psycopg2
+import os 
+from dotenv import load_dotenv
+
+load_dotenv()
+
+def insert_data(current_date, current_time, price ):
+    connection = psycopg2.connect(
+      dbname="pagila",
+      user= os.getenv('sql_user'),
+      password=os.getenv('sql_password'),
+      host=os.getenv('host'),
+      port="5432"
+        )
+    cursor = connection.cursor()
+    insert_statement = (f"""
+            INSERT INTO main.duckworld (pricedate, pricetime, price)
+            values ('{current_date}', '{current_time}', {price});            
+        """)      
+    cursor.execute(insert_statement)
+    connection.commit()
+    cursor.close()
+    connection.close()
+
+duckworld = os.getenv('duckworld')
+response = requests.get(duckworld).json()
+price = response['price']
+current_date = (timedelta(hours=1) + datetime.now()).date().strftime("%Y-%m-%d")
+current_time = (timedelta(hours=1) + datetime.now()).time().strftime("%H:%M:%S")
+
+insert_data(current_date, current_time, price)
